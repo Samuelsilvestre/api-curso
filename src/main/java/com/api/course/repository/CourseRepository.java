3d3bad09ba8b930123264086831a4f49a08384a6
Samuelsilvestre <fnndc90@gmail.com>
2023-12-01 17:32:27 -0300

    add repository

---
+++ src/main/java/com/api/course/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.api.course.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.course.models.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
