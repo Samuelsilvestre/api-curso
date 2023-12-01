00f869feda1680486d4f3915931b3bb2686c2152
Samuelsilvestre <fnndc90@gmail.com>
2023-12-01 17:29:01 -0300

    add table

---
+++ src/main/java/com/api/course/models/Course.java
@@ -0,0 +1,27 @@
+package com.api.course.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 25, nullable = false)
+    private String category;
+
+    @Column(length = 200, nullable = false)
+    private String description;
+    
+}
