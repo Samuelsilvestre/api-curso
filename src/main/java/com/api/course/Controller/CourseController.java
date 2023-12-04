123bdcab9c836b1b8610f8540039745f323840ec
Samuelsilvestre <fnndc90@gmail.com>
2023-12-04 08:24:17 -0300

    add method findbyid

--- src/main/java/com/api/course/Controller/CourseController.java
+++ src/main/java/com/api/course/Controller/CourseController.java
@@ -1,6 +1,9 @@
 package com.api.course.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +13,20 @@ import com.api.course.models.Course;
 import com.api.course.repository.CourseRepository;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api")
 public class CourseController {
     
     @Autowired
     private CourseRepository repository;
+   
+    @GetMapping("/get/course/{id}")
+    public ResponseEntity<Course> getId(@PathVariable Long id) {
+        return repository.findById(id)
+        .map(c -> ResponseEntity.ok().body(c))
+        .orElse(ResponseEntity.notFound().build());
+    }
 
-    @RequestMapping(value = "HTTP_CREATED")
-    @PostMapping("post/course")
+    @PostMapping("/post/course")
     public Course createCourse(@RequestBody Course course) {
         return repository.save(course);
     }
--- src/main/java/com/api/course/models/Course.java
+++ src/main/java/com/api/course/models/Course.java
@@ -23,5 +23,37 @@ public class Course {
 
     @Column(length = 200, nullable = false)
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
 }
