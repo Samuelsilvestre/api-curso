27c764fc16e319b9b2f6e190b45e2928f9913cb5
Samuelsilvestre <fnndc90@gmail.com>
2023-12-04 10:49:40 -0300

    add method update

--- src/main/java/com/api/course/Controller/CourseController.java
+++ src/main/java/com/api/course/Controller/CourseController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,18 @@ public class CourseController {
     @PostMapping("/post/course")
     public Course createCourse(@RequestBody Course course) {
         return repository.save(course);
+    }
+
+    @PutMapping("/put/course/{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course course) {
+        return repository.findById(id)
+        .map(record -> {
+            record.setTitle(course.getTitle());
+            record.setCategory(course.getCategory());
+            record.setDescription(course.getDescription());
+            Course courseUpdate = repository.save(record);
+            return ResponseEntity.ok().body(courseUpdate);
+        })
+        .orElse(ResponseEntity.notFound().build());
     }
 }
